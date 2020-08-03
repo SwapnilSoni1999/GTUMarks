d2eb1a5a5b952dc3d3984ae3f17422b971f2c251
NishargShah <nishargshah3101@gmail.com>
2020-08-03 12:44:05 +0530

    solved results boxes problem

--- components/ResultsTable.vue
+++ components/ResultsTable.vue
@@ -4,43 +4,47 @@
     <div class="col">
       <div class="box">
         <div class="row">
-          <div class="col-md-12">
-            <!-- Current Sem. Backlog -->
-            <div class="input-group my-3">
+          <!-- Current Sem. Backlog -->
+          <div class="col-12 col-sm-6 col-lg-3">
+            <div class="input-group my-2">
               <div>
-                <label class="input-group-text w-100" for="session">Current Semester Backlog</label>
+                <label class="input-group-text">Current Semester Backlog</label>
               </div>
               <input type="text" class="form-control student_data" :value="currSemBack" disabled />
             </div>
           </div>
-          <div class="col-6">
-            <!-- Total Backlog -->
-            <div class="input-group mt-3 mb-md-3">
+          <!-- Total Backlog -->
+          <div class="col-6 col-sm-6 col-lg-3">
+            <div class="input-group my-2">
               <div>
-                <label class="input-group-text" for="session">Total Backlog</label>
+                <label class="input-group-text">Total Backlog</label>
               </div>
               <input type="text" class="form-control student_data" :value="totalBacks" disabled />
             </div>
-            <!-- SPI -->
-            <div class="input-group my-3">
+          </div>
+          <!-- SPI -->
+          <div class="col-6 col-sm-4 col-lg-2">
+            <div class="input-group my-2">
               <div>
-                <label class="input-group-text" for="session">SPI</label>
+                <label class="input-group-text">SPI</label>
               </div>
               <input type="text" class="form-control student_data" :value="spi" disabled />
             </div>
           </div>
-          <div class="col-6">
-            <!-- CPI -->
-            <div class="input-group my-3">
+          <!-- CPI -->
+          <div class="col-6 col-sm-4 col-lg-2">
+            <div class="input-group my-2">
               <div>
-                <label class="input-group-text" for="session">CPI</label>
+                <label class="input-group-text">CPI</label>
               </div>
               <input type="text" class="form-control student_data" :value="cpi" disabled />
             </div>
-            <!-- CGPA -->
-            <div class="input-group my-3">
+          </div>
+          <!-- CGPA -->
+          <div class="col-6 col-sm-4 col-lg-2">
+            <div class="input-group my-2">
               <div>
-                <label class="input-group-text" for="session">CGPA</label>
+                <label class="input-group-text">CGPA</label>
               </div>
               <input type="text" class="form-control student_data" :value="cgpa" disabled />
             </div>
@@ -69,40 +73,29 @@ export default {
 
 <style scoped>
 .input-group-text {
-  width: 7.5rem; /* inherited */
+  width: 7.5rem;
   justify-content: center;
   text-align: center;
   border-top-right-radius: 0;
   border-bottom-right-radius: 0;
 }
 
-.student_info .input-group input {
-    min-width: 7.5rem; /* inherited */
+.student_info .input-group {
+  display: flex;
+  flex-direction: column;
 }
 
 .student_info .input-group input {
-    border: 1px solid #ccc;
-    text-align: center;
+  border: 1px solid #ccc;
+  text-align: center;
+  width: 100%;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
 }
 
-
-@media (max-width: 610px) {
-  .student_info .input-group {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .student_info .input-group label {
-    width: 100%;
-    border-bottom-left-radius: 0;
-    border-bottom-right-radius: 0;
-    white-space: normal;
-  }
-
-  .student_info .input-group input {
-    width: 100%;
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-  }
+.student_info .input-group label {
+  width: 100%;
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
 }
 </style>
